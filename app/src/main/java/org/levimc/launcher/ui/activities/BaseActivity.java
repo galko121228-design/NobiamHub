64b3656f0d49f67ced3379c29bf237b119c3c781
galko121228-design <galko121228@gmail.com>
2026-07-28 17:54:47 +0500

    BaseActivity: View instead of TextView, safe cast

--- app/src/main/java/org/levimc/launcher/ui/activities/BaseActivity.java
+++ app/src/main/java/org/levimc/launcher/ui/activities/BaseActivity.java
@@ -156,13 +156,16 @@ public class BaseActivity extends AppCompatActivity {
         int accent = pm.getAccentColor();
 
         for (int id : tabIds) {
-            TextView tab = findViewById(id);
+            View tab = findViewById(id);
             if (!(tab instanceof TextView)) continue;
             if (tab == null) continue;
             int color = getResources().getColor(R.color.text_secondary, getTheme());
-            tab.setTextColor(color);
-            tab.setTypeface(tab.getTypeface(), android.graphics.Typeface.NORMAL);
-            TextViewCompat.setCompoundDrawableTintList(tab, ColorStateList.valueOf(color));
+            if (tab instanceof TextView) {
+                TextView tv = (TextView) tab;
+                tv.setTextColor(color);
+                tv.setTypeface(tv.getTypeface(), android.graphics.Typeface.NORMAL);
+                TextViewCompat.setCompoundDrawableTintList(tv, ColorStateList.valueOf(color));
+            }
         }
 
         if (pm.hasBackgroundImage()) {
